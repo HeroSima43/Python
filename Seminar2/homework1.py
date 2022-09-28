70f6e32df9ab1ec07b93fe0f1b1731a27bf9d6c3
HeroSima43 <pwd@list.ru>
2022-09-28 21:59:37 +0500

    add homework1

---
+++ Seminar2/homework1.py
@@ -0,0 +1,19 @@
+#  Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+#  Пример:
+#  - 6782 -> 23
+#  - 0,56 -> 11
+number = float(input('Введите вещественное число: '))
+z = str(number)
+print(z)
+list = []
+for i in range(len(z)):
+    list.append(z[i])   
+print('Заполненный массив из строк:', list) 
+for i in list:
+    if i == '.':
+        list.remove(i)
+print('Измененный массив:', list)
+sum = 0
+for i in list:
+    sum += int(i)
+print(f'Сумма цифр числа {number} равна:', sum)
