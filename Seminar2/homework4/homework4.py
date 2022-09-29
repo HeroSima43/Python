c344232d59bbe9a412cea8eea7a0c6d4684782c6
HeroSima43 <pwd@list.ru>
2022-09-29 20:55:31 +0500

    add homework4

---
+++ Seminar2/homework4/homework4.py
@@ -0,0 +1,18 @@
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
+# Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+
+import random
+
+N = int(input('Введите N: '))
+list = []
+for i in range(N):
+    list.append(random.randint(-N, N))
+print(list)
+
+pro = 1
+
+data = open('C:/Users/pwd/Desktop/MyGeekBrainsProject/Python/Seminar2/homework4/file.txt', 'r')
+for i in data:
+    pro = pro * list[int(i)]
+print(f'Произведение элементов {list[1]} и {list[3]} равно: {pro}')
+data.close()
