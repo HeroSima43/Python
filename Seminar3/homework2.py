4db9cd72e3670d1520e4c0e0a07a5f027d5e99e5
HeroSima43 <pwd@list.ru>
2022-10-03 21:18:22 +0500

    add PEP8

--- Seminar3/homework2.py
+++ Seminar3/homework2.py
@@ -1,4 +1,5 @@
-# Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 # Пример:
 # - [2, 3, 4, 5, 6] => [12, 15, 4];
 # - [2, 3, 5, 6] => [12, 15]
@@ -8,7 +9,7 @@ import random
 N = int(input('Введите количество элементов массива: '))
 my_list = []
 for i in range(N):
-    my_list.append(random.randint(0,9))
+    my_list.append(random.randint(0, 9))
 print(my_list)
 
 new_list = []
@@ -17,7 +18,7 @@ y = N - 1
 
 if N % 2 == 0:
     for i in my_list:
-        if x >= y: 
+        if x >= y:
             break
         else:
             new_list.append(i * my_list[y])
@@ -26,7 +27,7 @@ if N % 2 == 0:
 if N % 2 == 1:
     for i in my_list:
         if x >= y:
-            new_list.append(i) 
+            new_list.append(i)
             break
         else:
             new_list.append(i * my_list[y])
