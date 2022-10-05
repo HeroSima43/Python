0b91927eeba42caeeefed1b15c6f86419900687e
HeroSima43 <pwd@list.ru>
2022-10-05 20:10:27 +0500

    add homework2

---
+++ Seminar4/homework2.py
@@ -0,0 +1,14 @@
+# Задайте натуральное число N.
+# Напишите программу, которая составит список простых множителей числа N.
+# Например: 54 -> [2, 3, 3, 3]
+
+N = 650
+my_list = []
+factor = 2
+while N > 1:
+    if N % factor == 0:
+        my_list.append(factor)
+        N = N / factor
+    else:
+        factor += 1
+print(my_list)
