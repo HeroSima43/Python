2b19a83eba17de957f6ef09d7a6fedabcead5518
HeroSima43 <pwd@list.ru>
2022-10-11 20:07:38 +0500

    add task1

---
+++ Seminar5/task1.py
@@ -0,0 +1,10 @@
+# В файле находится N натуральных чисел, записанных через пробел.
+# Среди чисел не хватает одного, чтобы выполнялось условие
+# A[i] - 1 = A[i-1]. Найдите это число.
+
+my_str = '1 2 3 4 5 6 8 9'
+my_list = list(map(int, my_str.split()))
+print(my_list)
+for i in range(1, len(my_list)):
+    if my_list[i]-1 != my_list[i-1]:
+        print(my_list[i-1]+1)
